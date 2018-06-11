d791a1b97b745b178d36fbdd7888cec18c383ba0
Satyanarain <satya2000chauhan@gmail.com>
2018-06-11 12:53:50 +0530

    modified

--- resources/views/requests/cash/cash_approver.blade.php
+++ resources/views/requests/cash/cash_approver.blade.php
@@ -80,8 +80,8 @@
                                              <input  type="hidden"  name="id" value="{{$requests->id}}">
                                              <input  type="hidden"  name="user_id" value="{{$requests->user_id}}">
                                               <input  type="hidden"  name="category_id" value="{{$requests->category_id}}">
-                                              <button class="btn btn-primary submit" type="submit" name="approve"  value="Approve"  onclick="return loadAddApprove()" style="margin-right:40px;"><i class="fa fa-check-circle"></i> Approve</button>
-                                            <button class="btn btn-danger submit" type="submit" name="approverejected" value="Rejected" onclick="return loadAdd()"><i class="fa fa-times-circle" aria-hidden="true"></i>
+                                              <button class="btn btn-primary submit pull-left" type="submit" name="approve"  value="Approve"  onclick="return loadAddApprove()" style="margin-right:40px;"><i class="fa fa-check-circle"></i> Approve</button>
+                                            <button class="btn btn-danger submit pull-right" type="submit" name="approverejected" value="Rejected" onclick="return loadAdd()"><i class="fa fa-times-circle" aria-hidden="true"></i>
                                                 Constraints</button>                                            
                                         </div>
 
