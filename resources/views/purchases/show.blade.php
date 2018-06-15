621c36e8bdd6092e405c62c8e881e472beb7d349
Satyanarain <satya2000chauhan@gmail.com>
2018-06-15 17:47:42 +0530

    modified

--- resources/views/purchases/show.blade.php
+++ resources/views/purchases/show.blade.php
@@ -25,75 +25,63 @@
                     'onsubmit'=>'return validatePan()',
                     'autocomplete'=>'off',
                     'class'=>'formValidate', 'files'=>true])!!}
-                      <table width="100%" cellspacing="4" cellpadding="4" >
+                      <table>
                         <tr>
                             <td  align="left"  style="text-align:center; background-color:#f2f2f2; border-bottom:#f8f8f8f8 ipx solid;" colspan="4">   
                                 {{Html::image('/images/logonicons/csei-60x60.png',array('style'=>''))}}</td>
                         </tr>
-                    <tr height="30px;" align="left" valign="top">
+                    <tr height="30px;" align="left" valign="top" >
                             <td width="45%"  style="bgcolor:#ccc;">
-                                <table width="100%" align="left" style="text-align:left;">
+                                <table width="100%" align="left" style="text-align:left;"  class="table table-bordered  table-striped table-hover bank_table">
 
-                                    <tr class="table-row">
-                                        <td width="50%" align="left"  style="text-align:left;"></td>
-                                    </tr>
-                                    <tr class="table-row" style="text-align:left; padding-bottom:20px;">
-                                        <td width="50%" align="left" style="text-align:left; padding:10px 0px 10px 0px;">  
-                                            Phone   : 011 2570 5650<br>
-                                            Website : http://csei.org.in/<br>
-                                            Address : Suman Lata Bhadola Marg, Block W, Guru Arjun Nagar, Shadipur, New Delhi, Delhi 110008
-                                        </td>
+                                    <tr class="table-row" style="text-align:left; padding-bottom:20px;" >
+                                                    <td align="left" valign="top" style="text-align:left">Phone</td><td align="left" valign="top" style="text-align:left">:</td>
+                                                   <td align="left" valign="top" style="text-align:left"> 011 2570 5650</td> 
+                                                   </tr>    
+                                                   <tr> <td align="left" valign="top" style="text-align:left">Website</td><td align="left" valign="top" style="text-align:left">:</td><td align="left" valign="top" style="text-align:left">http://csei.org.in/</td> </tr>    
+                                                   <tr> <td align="left" valign="top" style="text-align:left">Address</td><td align="left" valign="top" style="text-align:left">:</td><td align="left" valign="top" style="text-align:left">Suman Lata Bhadola Marg, Block W, Guru Arjun Nagar, Shadipur, New Delhi, Delhi 110008</td>    
+                                                 </tr>
+                                           </td>
                                     </tr>
                                 </table>
                             </td> 
                             <td width="10%">&nbsp;<td>
                                 <!--------------------------------ship to--------------------------->
                             <td width="45%" align="left" valign="top"> 
-                                <table width="100%" align="left" vlign="top" style="text-align:left;" border="0">
-                                    <tr class="table-row" >
-                                        <td  align="left" vlign="top"  style="text-align:left;color:#00aff0" colspan="2">   
-                                            <h4  style="text-align:left;color:#00aff0"></h4></td>
-                                    </tr>
-                                    <tr height="90" class="table-row" ><td width="20%" align="left" style="text-align:left;" > P. C.  S| No.</td>
-                                        <td style="padding-top: 0px;text-align:left;" align="left" vlign="top">
-                                            {{$total}}
-                                            {!! Form::hidden('po_number', $total, ['class' => 'form-control','style'=>"height:32px;"]) !!}
-
-                                        </td>
+                                <table  class="table table-bordered  table-striped table-hover bank_table">
+                                
+<tr height="90" class="table-row" style="text-align:left; padding-bottom:20px;"><td width="30%" align="left" style="text-align:left;" > P. C. S | No.</td>
+<td style="padding-top: 0px;text-align:left;" align="left" vlign="top"> {{$total}}{!! Form::hidden('po_number', $total, ['class' => 'form-control','style'=>"height:32px;"]) !!}</td>
                                     </tr> 
                                     <tr>
-                                        <td width="20%" align="left" vlign="top" style="text-align:left;" > Date </td>
-                                        <td style="text-align:left;" align="left" vlign="top">
-                                            <?php
-                                            echo $date = date("d-m-y");
-                                            ?>
-                                            {!! Form::hidden('date', $date, ['class' => 'form-control','style'=>"height:32px;"]) !!}
+<td width="20%" align="left" vlign="top" style="text-align:left;" > Date </td><td style="text-align:left;" align="left" vlign="top"><?php echo $date = date("d-m-y");
+?>{!! Form::hidden('date', $date, ['class' => 'form-control','style'=>"height:32px;"]) !!}
                                         </td>
                                     </tr>
                                 </table>
-
-                            </td>   
+   </td>   
 
                         </tr>
 
 
                     </table>  
 
-                    <table width="100%" align="left" style="text-align:left;" border="0">
+                    <table width="100%" align="left" style="text-align:left;" border="0" class="table table-bordered  table-striped table-hover bank_table">
                         <tr>
-                            <td align="left"  style="text-align:left; height:30px; width:50%"></td>
-                            <td align="left"  style="text-align:left; height:30px;">&nbsp</td>
+                            <td align="left"  style="text-align:left; height:0px; width:45%"><b>To</b></td>
+                            <td align="left"  style="text-align:left; height:0px;">&nbsp</td>
                         </tr>
-                        <tr height="14px;">
-                            <td align="left"  style="text-align:left;">To</td>
-                            <td>&nbsp;</td>
-                        </tr>
-                        <tr  style="text-align:left; padding-bottom:20px;">
+                       <tr  style="text-align:left; padding-bottom:20px;">
 
                             <td  align="left" style="text-align:left; padding:10px 0px 10px 0px;">
-                                Name    : {{$vendor_quotation_lists->name}}
-                                Phone   : {{$vendor_quotation_lists->name}}<br>
-                                Address : {{$vendor_quotation_lists->address}}
+                                
+                                 <table >
+                                             <td align="left" valign="top" style="text-align:left">Name</td><td align="left" valign="top" style="text-align:left">:</td><td align="left" valign="top" style="text-align:left">{{$vendor_quotation_lists->name}}</td> </tr>    
+                                                   <tr> <td align="left" valign="top" style="text-align:left">Phone</td><td align="left" valign="top" style="text-align:left">:</td><td align="left" valign="top" style="text-align:left"> {{$vendor_quotation_lists->name}}</td> </tr>    
+                                                   <tr> <td align="left" valign="top" style="text-align:left">Address</td><td align="left" valign="top" style="text-align:left">:</td><td align="left" valign="top" style="text-align:left">{{$vendor_quotation_lists->address}}</td>    
+                                                 </tr>
+                                             </table>
+                                
                             </td>
                             <td>&nbsp;</td>
                         </tr>
